d2c1ab1bd1d8f1495913e6697139ec32da591bff
gustavoclay <gustavoclay@gmail.com>
2024-09-18 20:46:00 -0300

    Criando página filmes populares e página detalhes dos filmes

--- 05-protejo-filmes-revisao/src/app/filmes/page.js
+++ 05-protejo-filmes-revisao/src/app/filmes/page.js
@@ -1,8 +1,9 @@
 'use client'
 
 import { useEffect, useState } from "react";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import apiFilmes from "../apis/apiFilmes";
 import Pagina from "../components/Pagina";
-
 
 export default function Filmes() {
 
@@ -10,29 +11,43 @@ export default function Filmes() {
     // e mude na tela
     const [filmes, setFilmes] = useState([])
 
-
     // Efeito Colateral
     useEffect(() => {
-        console.log(filmes)
         // A requisição pra buscar os filmes
-
-        // Alterar o estado filmes
-
-        
+        buscarFilmes()
     }, [])
-    
+
+    async function buscarFilmes() {
+        const resultado = await apiFilmes.get("/movie/popular?language=pt-BR")
+        console.log(resultado.data.results)
+        // alterando o estado filmes para receber os filmes da requisição
+        setFilmes(resultado.data.results)
+    }
 
 
     return (
         <Pagina titulo="Filmes Populares">
 
-            <ul>
+            <Row md={4}>
                 {
                     filmes.map(filme => {
-                        return <li>{filme}</li>
+                        return (
+                            <Col className="py-2">
+                                <Card style={{ height: '100%' }}>
+                                    <Card.Img src={'https://image.tmdb.org/t/p/w500/' + filme.poster_path} />
+                                    <Card.Body>
+                                        <Card.Title>{filme.original_title}</Card.Title>
+                                        <p><b>Nota: {filme.vote_average} ⭐</b></p>
+                                    </Card.Body>
+                                    <Card.Footer className="text-end">
+                                        <Button href={"/filmes/" + filme.id}>Detalhes</Button>
+                                    </Card.Footer>
+                                </Card>
+                            </Col>
+                        )
                     })
                 }
-            </ul>
+            </Row>
 
         </Pagina>
     )
